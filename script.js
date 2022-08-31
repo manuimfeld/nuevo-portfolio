4745b4158988bad7f6dab96642fd4ed5ab4b8378
Manuimfeld <manu_jadir@hotmail.com>
2022-08-31 13:26:16 -0300

    Add description projects and animate

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let buttonMenu = document.getElementById('button-menu')
-let menu = document.getElementById('menu')
-let links = menu.querySelectorAll('.links-mobile')
-let header = document.getElementById('header')
-let hideMenu = false
+let buttonMenu = document.getElementById("button-menu");
+let menu = document.getElementById("menu");
+let links = menu.querySelectorAll(".links-mobile");
+let header = document.getElementById("header");
+let hideMenu = false;
 
-header.appendChild(menu)
+header.appendChild(menu);
 
 const displayMenu = () => {
-    if (hideMenu == false) {
-        menu.style.display = "block"
-        hideMenu = true
-    } else {
-        menu.style.display = "none"
-        hideMenu = false
-    }
-}
+  if (hideMenu == false) {
+    menu.style.display = "block";
+    hideMenu = true;
+  } else {
+    menu.style.display = "none";
+    hideMenu = false;
+  }
+};
 
-buttonMenu.addEventListener('click', () =>  {
-    displayMenu()
-})
+buttonMenu.addEventListener("click", () => {
+  displayMenu();
+});
 
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        displayMenu()
-    })
-})
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    displayMenu();
+  });
+});
